abc4553c2380ff322468e50497aa797567f94e34
MaxLazunin <Artseven@mail.ru>
2023-12-29 11:23:12 +0400

    ui_mod +

--- Functions/start_program.py
+++ Functions/start_program.py
@@ -1,55 +1,39 @@
-from tkinter import *
-
 from InputData import input_data
 from OutputData import output_data
-from Functions import functions
-from Functions.set_values import first_run
-from UI.classes import OutputFrame
-import sys
+from UI.classes import InputFrame, OutputFrame
 
 
 def start_program(inside_left_frame, inside_right_frame):
-    source_water = OutputFrame(inside_left_frame,
-                               anchor='w',
-                               calc='get',
-                               side='top',
-                               text='Вода, поступающая в аэротэнк',
-                               appointment='in',
-                               obj=input_data.source_water,
-                               obj_name='source_water')
-    technological_parameters = OutputFrame(inside_left_frame,
-                                           anchor='w',
-                                           calc='get',
-                                           side='top',
-                                           text='Технологические параметры',
-                                           appointment='in',
-                                           obj=input_data.technological_parameters,
-                                           obj_name='technological_parameters')
-    construction_parameters = OutputFrame(inside_left_frame,
+    source_water = InputFrame(inside_left_frame,
+                              anchor='w',
+                              calc='get',
+                              side='top',
+                              text='Вода, поступающая в аэротэнк',
+                              obj=input_data.source_water)
+    technological_parameters = InputFrame(inside_left_frame,
                                           anchor='w',
                                           calc='get',
                                           side='top',
-                                          text='Конструктивные параметры сооружения',
-                                          appointment='in',
-                                          obj=input_data.construction_parameters,
-                                          obj_name='construction_parameters')
-    consts = OutputFrame(inside_left_frame,
-                         anchor='w',
-                         calc='get',
-                         side='top',
-                         text='Кинетические константы',
-                         appointment='in',
-                         obj=input_data.constants,
-                         obj_name='constants')
+                                          text='Технологические параметры',
+                                          obj=input_data.technological_parameters)
+    construction_parameters = InputFrame(inside_left_frame,
+                                         anchor='w',
+                                         calc='get',
+                                         side='top',
+                                         text='Конструктивные параметры сооружения',
+                                         obj=input_data.construction_parameters)
+    consts = InputFrame(inside_left_frame,
+                        anchor='w',
+                        calc='get',
+                        side='top',
+                        text='Кинетические константы',
+                        obj=input_data.constants)
 
     output = OutputFrame(inside_right_frame,
                          anchor='n',
                          calc='set',
                          side='top',
                          text='Выходные параметры',
-                         appointment='out',
-                         label_heigh=400,
-                         obj=output_data.output_data,
-                         obj_name='output')
+                         obj=output_data.output_data)
 
     return source_water, technological_parameters, construction_parameters, consts, output
--- UI/classes.py
+++ UI/classes.py
@@ -3,8 +3,7 @@ from Functions.to_from_json import *
 from InputData.Constants import constants
 
 
-class OutputFrame:
-
+class MainFrame:
     def on_mousewheel(self, event):
         self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
@@ -22,12 +21,12 @@ class OutputFrame:
     #     # здесь ваш код для отображения графика в зависимости от значения
     #     print(f'Showing graph for value: {value}')
 
-    def __init__(self, master, appointment, anchor, obj, obj_name, label_heigh=150, calc=None,
+    def __init__(self, master, anchor,  obj, calc=None,
                  side='top', text='Главное окно программы'):
         self.obj = obj['value']
         self.obj_name = obj['name']
         self.calc = calc
-        self.main_frame = LabelFrame(master, text=text, width=600, height=label_heigh)
+        self.main_frame = LabelFrame(master, text=text, width=600, height=150)
         self.main_frame.pack(side=side, anchor=anchor, expand=True, fill='both')
         self.main_frame.pack_propagate(0)
 
@@ -61,33 +60,6 @@ class OutputFrame:
                 self.values.append(list(self.obj.values())[i]['value'])
                 self.descriptions.append(list(self.obj.values())[i]['description'])
                 self.mod.append(list(self.obj.values())[i]['mod'])
-        for i in range(len(self.names)):
-            frame = Frame(self.inner_frame, height=30)
-            frame.pack(anchor='w')
-            value = None
-
-            if appointment == 'in':
-                value = Entry(frame, width=20)
-                value.insert(0, round(float(self.values[i]), 3))
-                value.bind("<Down>", self.focus_next_widget)
-                value.bind("<Up>", self.focus_previous_widget)
-
-            elif appointment == 'out':
-                value_old = Label(frame, width=10, text='-')
-                self.values_list_old.append(value_old)
-                value_old.pack(side='left')
-                value = Label(frame, width=10, text=round(float(self.values[i]), 2))
-                setattr(self, f'entry_{self.names[i]}', value)
-                setattr(self, f'entry_{self.names[i]}_old', value_old)
-                # graph_btn = Button(frame, text='Show Graph', command=lambda v=value: self.show_graph(v))  # <- Добавить кнопку
-
-            self.labels_list.append(value)
-            description = Label(frame, anchor='w', text=self.descriptions[i])
-            value.pack(side='left')
-            setattr(self, f'entry_{self.names[i]}', value)
-            # if appointment == 'out':
-            #     graph_btn.pack(side='left')
-            description.pack(side='left', expand=True)
 
     def set_new_values(self):
         if self.calc == 'get':
@@ -106,6 +78,53 @@ class OutputFrame:
     #         self.obj = constants
 
     def show_output(self):
+        pass
+
+
+class InputFrame(MainFrame):
+
+    def __init__(self, master, anchor,  obj,  calc, side, text):
+        super().__init__(master, anchor,  obj,  calc, side, text)
+        for i in range(len(self.names)):
+            frame = Frame(self.inner_frame, height=30)
+            frame.pack(anchor='w')
+
+            value = Entry(frame, width=20)
+            value.insert(0, round(float(self.values[i]), 3))
+            value.bind("<Down>", self.focus_next_widget)
+            value.bind("<Up>", self.focus_previous_widget)
+
+            self.labels_list.append(value)
+            description = Label(frame, anchor='w', text=self.descriptions[i])
+            value.pack(side='left')
+            setattr(self, f'entry_{self.names[i]}', value)
+            description.pack(side='left', expand=True)
+
+
+class OutputFrame(MainFrame):
+    def __init__(self, master, anchor,  obj,  calc, side, text):
+        super().__init__(master, anchor,  obj,  calc, side, text)
+        for i in range(len(self.names)):
+            frame = Frame(self.inner_frame, height=30)
+            frame.pack(anchor='w')
+
+            value_old = Label(frame, width=10, text='-')
+            self.values_list_old.append(value_old)
+            value_old.pack(side='left')
+            value = Label(frame, width=10, text=round(float(self.values[i]), 2))
+            setattr(self, f'entry_{self.names[i]}', value)
+            setattr(self, f'entry_{self.names[i]}_old', value_old)
+            # graph_btn = Button(frame, text='Show Graph', command=lambda v=value: self.show_graph(v))  # <- Добавить кнопку
+
+            self.labels_list.append(value)
+            description = Label(frame, anchor='w', text=self.descriptions[i])
+            value.pack(side='left')
+            setattr(self, f'entry_{self.names[i]}', value)
+            # if appointment == 'out':
+            #     graph_btn.pack(side='left')
+            description.pack(side='left', expand=True)
+
+    def show_output(self):
         if self.calc == 'set':
             for i in range(len(self.labels_list)):
                 self.values_list_old[i]['text'] = self.labels_list[i]['text']
@@ -119,110 +138,3 @@ class OutputFrame:
 
     def change_slider(self, scale):
         self.obj['bod_full_source']['value'] = float(scale.get())
-
-
-# if __name__ == '__main__':
-#
-#     first_run()
-#     functions.calculate_zones()
-#     root = Tk()
-#     root.title('Главное окно')
-#     root.update_idletasks()
-#     s = root.geometry()
-#     s = s.split('+')
-#     s = s[0].split('x')
-#     width_root = int(s[0])
-#     height_root = int(s[1])
-#
-#     w = root.winfo_screenwidth()
-#     h = root.winfo_screenheight()
-#     w = w // 2
-#     h = h // 2
-#     w = w - width_root // 2
-#     h = h - height_root // 2
-#     root.geometry('1550x750+200+100'.format(w, h))
-#     inside_left_frame = Frame()
-#     inside_right_frame = Frame()
-#     inside_left_frame.pack(side='left', anchor='n', padx=20, fill='both', expand=True)
-#     inside_right_frame.pack(side='left', anchor='n', fill='both', expand=True)
-#     source_water = OutputFrame(inside_left_frame,
-#                                anchor='w',
-#                                calc='get',
-#                                side='top',
-#                                text='Вода, поступающая в аэротэнк',
-#                                appointment='in',
-#                                obj=input_data.source_water,
-#                                obj_name='source_water')
-#     technological_parameters = OutputFrame(inside_left_frame,
-#                                            anchor='w',
-#                                            calc='get',
-#                                            side='top',
-#                                            text='Технологические параметры',
-#                                            appointment='in',
-#                                            obj=input_data.technological_parameters,
-#                                            obj_name='technological_parameters')
-#     construction_parameters = OutputFrame(inside_left_frame,
-#                                           anchor='w',
-#                                           calc='get',
-#                                           side='top',
-#                                           text='Конструктивные параметры сооружения',
-#                                           appointment='in',
-#                                           obj=input_data.construction_parameters,
-#                                           obj_name='construction_parameters')
-#     consts = OutputFrame(inside_left_frame,
-#                          anchor='w',
-#                          calc='get',
-#                          side='top',
-#                          text='Кинетические константы',
-#                          appointment='in',
-#                          obj=input_data.constants,
-#                          obj_name='constants')
-#
-#     output = OutputFrame(inside_right_frame,
-#                          anchor='n',
-#                          calc='set',
-#                          side='top',
-#                          text='Выходные параметры',
-#                          appointment='out',
-#                          label_heigh=400,
-#                          obj=output_data.output_data,
-#                          obj_name='output')
-#
-#     # output = OutputFrame(inside_right_frame,
-#     #                      anchor='n',
-#     #                      calc='set',
-#     #                      side='top',
-#     #                      text='Выходные параметры',
-#     #                      appointment='out',
-#     #                      label_heigh=400,
-#     #                      obj=output_data.output_results,
-#     #                      obj_name='output')
-#     #
-#     # pwqr = OutputFrame(inside_right_frame,
-#     #                    anchor='n',
-#     #                    calc='set',
-#     #                    side='top',
-#     #                    text='Качество очищенной воды',
-#     #                    appointment='out',
-#     #                    label_heigh=100,
-#     #                    obj=output_data.purified_water_quality_results,
-#     #                    obj_name='output')
-#
-#     button = Button(inside_right_frame,
-#                     text='Расчитать',
-#                     command=lambda: functions.calculate(source_water, consts,
-#                                                         construction_parameters,
-#                                                         technological_parameters,
-#                                                         output),
-#                     activebackground='gray'
-#                     )
-#     button.pack(side='top', anchor='w')
-#
-#
-#     root.mainloop()
-
-
-
-
-
-
